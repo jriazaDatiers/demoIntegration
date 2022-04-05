9075105609dd0f3b06cac84b8ef9ea455d5ff602
JMR <jriaza@datiers.com>
2022-04-05 12:16:49 +0200

    base no tags

--- src/test/java/StepDefinitions/RunCucumberTest.java
+++ src/test/java/StepDefinitions/RunCucumberTest.java
@@ -6,6 +6,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin={"pretty"},features = "src/test/resources/features",tags = "@ReadOnly")
+@CucumberOptions(plugin={"pretty"},features = "src/test/resources/features")
 public class RunCucumberTest {
 }
